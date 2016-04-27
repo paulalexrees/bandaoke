5b74062b61373ba2c330d81da12fee29385a11ad
Tobenna <tobennandu@gmail.com>
2016-04-27 12:30:42 +0100

    Refactored tests

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,21 +7,20 @@ class Song
 
   has n, :roles
 
-  def initialize(title: , artist:)
-    self.title = title
-    self.artist = artist
-    self.save
-    self.attach_default_roles
+  def self.add_with_roles(title:, artist:)
+    song = self.create(title: title, artist: artist)
+    song.attach_default_roles
   end
 
-  def is_complete?
+  def complete?
     self.roles.first(player: nil) == nil && self.roles.length != 0
   end
 
   def attach_default_roles
-    DEFAULT_ROLES.each do |instrument|
+    DEFAULT_ROLES.map do |instrument|
       create_song_role(instrument)
     end
+    self
   end
 
   private
@@ -29,4 +28,5 @@ class Song
   def create_song_role(instrument)
     self.roles.create(instrument: instrument)
   end
+
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,19 +1,26 @@
 describe Song do
 
-  let(:song)  { described_class.new(title: "Enter Sandman", artist: "Metallica") }
   let(:number_default_roles) { described_class::DEFAULT_ROLES.size }
-  it 'starts as incomplete' do
-    expect(song.is_complete?).to be false
+  let(:Song) { described_class }
+  describe '#complete?' do
+
+    let(:song) { described_class.new(title: "Enter Sandman", artist: "Metallica") }
+
+    it 'starts as incomplete' do
+      expect(song).to_not be_complete
+    end
+
+    it 'can be marked complete' do
+      role = song.roles.new(instrument: "Guitar")
+      role.fill("James Hetfield")
+      expect(song).to be_complete
+    end
   end
 
-  it 'can be marked complete' do
-    role = song.roles.new(instrument: "Guitar")
-    role.fill("James Hetfield")
-    expect(song.is_complete?).to be true
-  end
-
-  it 'attaches the default roles' do
-    expect(song.roles).to receive(:create).exactly(number_default_roles).times
-    song.attach_default_roles
+  describe '#add_with_roles' do
+    let(:song) { Song.add_with_roles(title: "Enter Sandman", artist: "Metallica") }
+    it 'attaches the default roles' do
+      expect(song.roles.size).to eq(number_default_roles)
+    end
   end
 end
