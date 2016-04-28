9c2dbaaa27a2023543ef3109de7183e6eb4511a2
Tobenna <tobennandu@gmail.com>
2016-04-28 11:46:34 +0100

    Finishing angular setup

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -20,12 +20,12 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: '/app/views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
       .when('/about', {
-        templateUrl: 'views/about.html',
+        templateUrl: '/app/views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
