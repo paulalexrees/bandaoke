6b4a421001235b2e5bab179395184039f95013c8
Tobenna <tobennandu@gmail.com>
2016-04-29 09:37:49 +0100

    Strangely protractor not working

--- public/test/e2e/songFeatures.js
+++ public/test/e2e/songFeatures.js
@@ -23,13 +23,19 @@ describe('Adding songs', function() {
   it('returns a list of matched songs', function () {
     var songs = $$('#songs .song');
     firstSongTitle = songs.first().element(by.css(".title"));
+    browser.pause();
     expect(firstSongTitle.getText()).toMatch('fake-song');
     secondSongTitle = songs.last().element(by.css(".title"));
+
     expect(secondSongTitle.getText()).toMatch('fake-song2');
   });
 
   it('adds a song to the songs database', function () {
   var firstSong = $$('#songs .song').first();
   firstSong.click();
+  });
+
+  afterAll(function(){
+    mock.teardown();
   });
 });
